806ef810dc43307b0bc49b0a49226ef40f5d268b
powerex <flaxazbest@gmail.com>
2017-04-05 21:54:04 +0300

    Choice box added

--- src/sample/PersonalController.java
+++ src/sample/PersonalController.java
@@ -4,11 +4,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 
 
+import javax.swing.event.DocumentEvent;
 import java.io.File;
 import java.io.IOException;
 
@@ -22,7 +22,26 @@ public class PersonalController {
     RadioButton female;
 
     @FXML
+    ChoiceBox<String> choice;
+    @FXML
+    ComboBox<String> combo;
+
+    @FXML
     public void initialize() throws IOException {
+
+        choice.getItems().add("Something");
+        choice.getItems().addAll("Something else", "Anytjing");
+        choice.setValue("Something");
+
+        combo.getItems().addAll("a", "b", "c");
+        combo.setEditable(false);
+        combo.setOnKeyPressed(e->{
+            if (e.getCode() == KeyCode.ENTER) {
+                String text = combo.getValue();
+                combo.getItems().add(text);
+                combo.setValue("");
+            }
+        });
 
         ToggleGroup tg = new ToggleGroup();
         male.setToggleGroup(tg);
